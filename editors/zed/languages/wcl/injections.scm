494a9e147c5601465cbedff4417c434124c9839d
Wil Taylor <cert@wiltaylor.dev>
2026-04-06 22:30:45 +1000

    refactor: drop Zed editor support
    Zed-specific extension, packaging, CI job, docs sections, and marketing
    site card are all removed. Zed users can still talk to `wcl lsp`
    through Zed's generic LSP client \u2014 we just don't maintain a dedicated
    extension anymore.

--- editors/zed/languages/wcl/injections.scm
+++
@@ -1,7 +0,0 @@
-; Injection queries for WCL
-; Used by Neovim and other tree-sitter hosts for embedded language support.
-
-; Interpolation expressions inside strings get WCL highlighting
-(interpolation
-  (expression) @injection.content
-  (#set! injection.language "wcl"))
